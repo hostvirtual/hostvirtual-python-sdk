2ce3fe6874c3f613ccee9f58c0c7e36899e5328a
Dennis Durling <djdtahoe@gmail.com>
2019-05-05 22:58:07 -0700

    not sure

--- hostvirtual.py
+++ hostvirtual.py
@@ -45,7 +45,8 @@ def connection(key, api_version):
             elif method == 'POST':
                 response = requests.post(url_root, json=data)
         except requests.HTTPError:
-            raise HostVirtualException(response.status_code, response.content)
+            raise HostVirtualException(
+                response.status_code, response.content)
 
         return response
 
@@ -61,29 +62,29 @@ class HostVirtualNodeDriver():
         self.api_version = api_version
         self.key = key
         self.connection = connection(self.key, api_version=api_version)
-    
+
     def locations(self):
         return self.connection('/cloud/locations/')
 
     def os_list(self):
         return self.connection('/cloud/images/')
-    
+
     def plans(self, location=False):
         if location:
             return self.connection('/cloud/sizes/' + str(location))
         else:
             return self.connection('/cloud/sizes/')
-    
+
     def servers(self, mbpkgid=False):
         if mbpkgid:
             result = self.connection('/cloud/server/' + str(mbpkgid))
         else:
             result = self.connection('/cloud/servers/')
         return result
-    
+
     def packages(self):
         return self.connection('/cloud/packages')
-    
+
     def package(self, mbpkgid):
         return self.connection('/cloud/package/' + str(mbpkgid))
 
@@ -92,24 +93,24 @@ class HostVirtualNodeDriver():
 
     def ipv6(self, mbpkgid):
         return self.connection('/cloud/ipv6/' + str(mbpkgid))
-    
+
     def networkips(self, mbpkgid):
         return self.connection('/cloud/networkips/' + str(mbpkgid))
-    
+
     def summary(self, mbpkgid):
         return self.connection('/cloud/serversummary/' + str(mbpkgid))
 
     def start(self, mbpkgid):
         return self.connection(
             '/cloud/server/start/{}'.format(mbpkgid), method='POST')
-    
+
     def shutdown(self, mbpkgid, force=False):
         params = {}
         if force:
             params['force'] = 1
         return self.connection(
             '/cloud/server/shutdown/{}'.format(mbpkgid), data=params, method='POST')
-    
+
     def reboot(self, mbpkgid, force=False):
         params = {}
         if force:
@@ -117,7 +118,7 @@ class HostVirtualNodeDriver():
         return self.connection(
             '/cloud/server/reboot/{}'.format(mbpkgid), data=params,
             method='POST')
-    
+
     def rescue(self, mbpkgid, password):
         params = {'rescue_pass': str(password)}
         return self.connection(
@@ -147,6 +148,12 @@ class HostVirtualNodeDriver():
     def status(self, mbpkgid):
         return self.connection('/cloud/status/{0}'.format(mbpkgid))
 
+    # TODO
+
+    # root password call is not enabled for api key use yet,
+    # however you can auth with the account password to verify & submit
+    # params['email']= 'abc@cde.com'
+    # params['password']= 'abc!@#'
     def bandwidth_report(self, mbpkgid):
         return self.connection('/cloud/servermonthlybw/' + str(mbpkgid))
 
@@ -164,12 +171,6 @@ class HostVirtualNodeDriver():
 
         return self.connection(
             '/cloud/buy_build/', data=params, method='POST')
-
-    # TODO
-    # root password call is not enabled for api key use yet,
-    # however you can auth with the account password to verify & submit
-    # params['email']= 'abc@cde.com'
-    # params['password']= 'abc!@#'
 
     def root_password(self, mbpkgid, passwd):
         params = {'rootpass': passwd}
